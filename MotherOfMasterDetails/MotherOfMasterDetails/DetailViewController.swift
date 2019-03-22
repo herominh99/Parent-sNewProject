45970073a7337ab3b42306c803e675fa744ec356
Minh Nguyễn <herominh99@gmail.com>
2019-03-22 15:42:44 +0700

    init

---
+++ MotherOfMasterDetails/MotherOfMasterDetails/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  MotherOfMasterDetails
+//
+//  Created by Nguyễn Minh on 3/22/19.
+//  Copyright © 2019 Nguyễn Minh. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
